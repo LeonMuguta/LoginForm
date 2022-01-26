d3beef4343b558177ed434dfd1ec096916acc8b3
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-01-26 11:53:17 +0200

    Added sign-up page

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get("/sign-up", function(req, res) {
+    res.sendFile(__dirname + '/sign-up.html');
+});
+
+// app.post("/", function(req, res) {
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     console.log(email);
+//     console.log(password);
+// });
+
+// app.post("/sign-up", function(req, res) {
+//     const password = req.body.password;
+//     const confirmPassword = req.body.confirmPassword;
+
+//     if (password !== confirmPassword) {
+//         console.log("Passwords are not the same!");
+//     }
+
+//     console.log(password);
+//     console.log(confirmPassword);
+// });
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
   });
